4f0f949d44898a218fdd0d49aa4a47c96b763730
takedasan <takedasan593@gmail.com>
2017-12-19 21:33:47 +0900

    Add WebJars

---
+++ src/main/java/jp/takeda/catform/CatformApplication.java
@@ -0,0 +1,12 @@
+package jp.takeda.catform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatformApplication.class, args);
+	}
+}
---
+++ src/main/java/jp/takeda/catform/config/WebConfig.java
@@ -0,0 +1,14 @@
+package jp.takeda.catform.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/webjars/**").addResourceLocations("/webjars/");
+	}
+}
