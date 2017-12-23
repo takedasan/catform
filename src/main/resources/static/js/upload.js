a45d8f34bf7951a03e23ba5aec35ecccfe1a1378
takedasan <takedasan593@gmail.com>
2017-12-23 18:28:52 +0900

    Add image upload pgae HTML

--- src/main/resources/static/js/upload.js
+++ src/main/resources/static/js/upload.js
@@ -2,8 +2,11 @@ ELEMENT.locale(ELEMENT.lang.ja)
 var form = new Vue({
 	el: '#input',
 	data: {
-		title: '',
-		date: ''
+		form: {
+			title: '',
+			date: '',
+			fileList2: [{ name: 'food.jpeg', url: 'https://fuss10.elemecdn.com/3/63/4e7f3a15429bfda99bce42a18cdd1jpeg.jpeg?imageMogr2/thumbnail/360x360/format/webp/quality/100' }, { name: 'food2.jpeg', url: 'https://fuss10.elemecdn.com/3/63/4e7f3a15429bfda99bce42a18cdd1jpeg.jpeg?imageMogr2/thumbnail/360x360/format/webp/quality/100' }]
+		}
 	},
 	methods: {
 		post: function () {
@@ -13,6 +16,18 @@ var form = new Vue({
 				data: JSON.stringify(this.date),
 				contentType: 'application/json'
 			});
+		},
+		handleRemove(file, fileList) {
+			console.log(file, fileList);
+		},
+		handlePreview(file) {
+			console.log(file);
+		},
+		handleOpen(key, keyPath) {
+			console.log(key, keyPath);
+		},
+		handleClose(key, keyPath) {
+			console.log(key, keyPath);
 		}
 	}
 })
